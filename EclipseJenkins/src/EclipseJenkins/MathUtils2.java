fac8e61cdbd67dd48288bc094a41bfd93e57e164
ThomasCGean <tgean1@toromail.csudh.edu>
2025-01-21 13:48:57 -0800

    Added a test string as output

---
+++ EclipseJenkins/src/EclipseJenkins/MathUtils2.java
@@ -0,0 +1,10 @@
+package EclipseJenkins;
+
+public class MathUtils2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test");
+	}
+
+}
