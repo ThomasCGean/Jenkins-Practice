716c4035e595d257e2c9f871c9d36dd5abe34e1e
ThomasCGean <tgean1@toromail.csudh.edu>
2025-01-21 15:25:07 -0800

    Added JUnit tests
    Added JUnit tests for the math utilities in the MathUtils2 class

---
+++ EclipseJenkins/src/EclipseJenkins/MathUtils2Test.java
@@ -0,0 +1,34 @@
+package EclipseJenkins;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtils2Test {
+
+	@Test
+	void testAddition() {
+		MathUtils2 testAdd = new MathUtils2();
+		int sum = testAdd.addition(10, 5);
+		assertEquals(15,sum);
+	}
+	@Test
+	void testSubtraction() {
+		MathUtils2 testSubt = new MathUtils2();
+		int difference = testSubt.subtraction(10, 5);
+		assertEquals(5, difference);
+	}
+	@Test
+	void testMult() {
+		MathUtils2 testMult = new MathUtils2();
+		int product = testMult.mult(2, 5);
+		assertEquals(10,product);
+	}
+	@Test
+	void testDiv() {
+		MathUtils2 testDivide = new MathUtils2();
+		double quotient = testDivide.divide(25, 5);
+		assertEquals(5,quotient);
+	}
+
+}
