04150ced46c4ee488e868e71e2c628afe32b8bbd
ThomasCGean <tgean1@toromail.csudh.edu>
2025-01-21 14:19:20 -0800

    Added math utilitites
    Added basic math operations in order to test them with Jenkins

--- EclipseJenkins/src/EclipseJenkins/MathUtils2.java
+++ EclipseJenkins/src/EclipseJenkins/MathUtils2.java
@@ -6,5 +6,27 @@ public class MathUtils2 {
 		// TODO Auto-generated method stub
 		System.out.println("This is a test");
 	}
-
+	public int addition(int a, int b) {
+		int k;
+		k = a + b;
+		return k;
+	}
+	public int subtraction(int a, int b) {
+		int k;
+		k = a - b;
+		return k;
+	}
+	public int mult(int a, int b) {
+		int k;
+		k = a * b;
+		return k;
+	}
+	public double divide(int a, int b) {
+		if (b == 0) {
+			return -1;
+		}
+		double k;
+		k = a / b;
+		return k;
+	}
 }
